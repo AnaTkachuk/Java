6fcf779d289a3df4b1207691e4fa330ac3ba6a13
AnaTkachuk <mizzzrka@mail.ru>
2018-09-24 15:55:40 +0300

    /w message

--- Controller.java
+++ Controller.java
@@ -1,36 +1,66 @@
-import javafx.fxml.Initializable;
+package ru.geek.core.client;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML
+    TextField textField;
+    @FXML
+    TextArea textArea;
 
     private Socket socket;
-    private DataInputStream in;
     private DataOutputStream out;
+    private DataInputStream in;
 
-    final String ServIP = "localhost";
-    final int ServPort = 4789;
+    public String nickname;
 
+
+    final String SERVER_IP = "localhost";
+    final int SERVER_PORT = 8189;
+
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(textField.getText());
+            textField.clear();
+            textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void wMsg(){
+
+        try {
+            out.writeUTF("/w " + nickname + " " + textField.getText());
+            textField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    try {
-        socket = new Socket(ServIP,ServPort);
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true){
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
                     try {
-                        String s = in.readUTF();
-
+                        while (true) {
+                            String s = in.readUTF();
+                            textArea.appendText(s + "\n");
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }finally {
@@ -41,13 +71,12 @@ public class Controller implements Initializable {
                         }
                     }
                 }
-            }
-        });
-        t1.setDaemon(true);
-        t1.start();
+            });
+            t.setDaemon(true);
+            t.start();
 
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ ServerMain.java
@@ -0,0 +1,25 @@
+package ru.geek.core.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        try( ServerSocket serverSocket = new ServerSocket(8189)){
+            System.out.println("Server started ... Waiting for clients");
+            while (true){
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected");
+                new ClientHandler(socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
